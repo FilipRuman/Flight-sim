73119bc975c39d8592d99f04cc6a83474ccac940
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2025-01-24 10:10:45 +0100

    Added display of flap for yaw alerone

--- Wings/Flap.cs
+++ Wings/Flap.cs
@@ -4,11 +4,16 @@ using System;
 public partial class Flap : Node3D
 {
 	[Export] Wing wing;
-
+	[Export] bool yaw;
 	public override void _Process(double delta)
 	{
 		if (wing.rotateWholeWing)
-			RotationDegrees = Vector3.Right * wing.RotationDegrees.X;
+		{
+			if (yaw)
+				RotationDegrees = Vector3.Up * wing.RotationDegrees.Y;
+			else
+				RotationDegrees = Vector3.Right * wing.RotationDegrees.X;
+		}
 		else
 			RotationDegrees = Vector3.Right * wing.flapAngle;
 	}
