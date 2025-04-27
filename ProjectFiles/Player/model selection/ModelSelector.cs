5dd8dfdff53732a650d6a192ce455742a9840832
FilipRuman <137614590+FilipRuman@users.noreply.github.com>
2025-04-27 18:43:12 +0200

    terrain gen working

--- ProjectFiles/Player/model selection/ModelSelector.cs
+++ ProjectFiles/Player/model selection/ModelSelector.cs
@@ -1,12 +1,10 @@
 using Godot;
 using System;
-
-public partial class ModelSelector : Node
-{
+using FilipRuman.ProceduralMapGen;
+public partial class ModelSelector : Node {
     [Export] Node3D spawnPoint;
-    [Export] TerrainGenController terrainGenController;
-    public void OnSelection(PackedScene model)
-    {
+    [Export] TerrainGenController terrainGen;
+    public void OnSelection(PackedScene model) {
 
         var node = model.Instantiate();
         var node3D = node as Node3D;
@@ -14,12 +12,10 @@ public partial class ModelSelector : Node
         /*         (node3D as RigidBody3D).LinearVelocity = Vector3.Forward * 100;
          */
         spawnPoint.AddChild(node);
-
-        terrainGenController.Player = node3D;
+        terrainGen.Player = node3D;
+        // terrainGenController.Player = node3D;
         foreach (var item in GetChildren())
-        {
             item.QueueFree();
-        }
         Dispose();
     }
 
